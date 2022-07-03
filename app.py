6b79e02be06fa67abe3c223fd951f2ac6ed51068
s1f10220103 <107385127+s1f10220103@users.noreply.github.com>
2022-07-03 23:40:36 +0900

    コメント更新

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ def answer():
         kinoko_count+=1
     elif request.form.get('item')=='takenoko':
         takenoko_count+=1
+    message.append(request.form.get('message'))
+    if len(message)>3:
+        message.pop(0)
+    message_html=''
+    for i in range(len(message)):
+        c='alert-warning ms-5' if i%2==0 else 'alert-success me-5'
+        m=message[i]
+        message_html+=f'<div class="alert{c}" role="alert">{m}</div>\n'
     kinoko_percent=kinoko_count/(kinoko_count+takenoko_count)*100
     takenoko_percent=takenoko_count/(kinoko_count+takenoko_count)*100
     return render_template('vote.html', **vars())
